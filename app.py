2ef67dd520fba0a1753f7ca02215c127a4266514
nishantprime <nishantkumarprime@gmail.com>
2024-12-11 16:16:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,24 +8,16 @@ app = Flask(__name__)
 @app.route('/', methods = ['GET', 'POST'])
 def chat():
   if request.method == 'GET':
-    chat = list(collection.find({},{'prompt':1, 'response':1, 'formatted_time':1}))
+    chat = list(collection.find({}, {'prompt':1, 'response':1, 'formatted_time':1, 'tokens':1}))
     return render_template('index.html', chat=chat)
   else:
-    chat = list(collection.find({}, {'prompt': 1, 'response': 1, 'formatted_time': 1}))
     prompt = request.form['prompt']
     time, response, token_usage = generation(prompt)
     if not time:
-      chat = list(collection.find({}, {'prompt': 1, 'response': 1, 'formatted_time': 1}))
+      chat = list(collection.find({}, {'prompt':1, 'response':1, 'formatted_time':1, 'tokens':1}))
       return render_template('index.html', chat=chat, error=response)
     # Create a new message to display (including the user's input)
-    new_message = {
-        'prompt': prompt,
-        'response': response,
-        'formatted_time': time,
-        'tokens': token_usage
-    }
-    chat.append(new_message)
-    
+    chat = list(collection.find({}, {'prompt':1, 'response':1, 'formatted_time':1, 'tokens':1}))
     # Render the template with the updated chat history
     return render_template('index.html', chat=chat)
 
