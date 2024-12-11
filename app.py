a14521aefe2604b64b52db24f4f607ba3ecce8a6
nishantprime <nishantkumarprime@gmail.com>
2024-12-11 16:30:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 from flask import Flask, request, render_template
-from database import collection
-from helper import generation
+from helper import generation, get_messages
 
 
 app = Flask(__name__)
 
+
+
 @app.route('/', methods = ['GET', 'POST'])
 def chat():
   if request.method == 'GET':
-    chat = list(collection.find({}, {'prompt': 1, 'response': 1, 'formatted_time': 1, 'tokens': 1}).sort('created',-1).limit(20))
+    chat = get_messages()
     
     return render_template('index.html', chat=chat)
   else:
     prompt = request.form['prompt']
     time, response, token_usage = generation(prompt)
     if not time:
-      chat = list(collection.find({}, {'prompt': 1, 'response': 1, 'formatted_time': 1, 'tokens': 1}).sort('created',-1).limit(20))
+      chat = get_messages()
       return render_template('index.html', chat=chat, error=response)
     # Create a new message to display (including the user's input)
-    chat = list(collection.find({}, {'prompt': 1, 'response': 1, 'formatted_time': 1, 'tokens': 1}).sort('created',-1).limit(20))
+    chat = get_messages()
     # Render the template with the updated chat history
     return render_template('index.html', chat=chat)
 
