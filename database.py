af5bb9cfd71fbee3b9f5c39b8488a989a802ee76
nishantprime <nishantkumarprime@gmail.com>
2024-12-11 10:32:52 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,9 @@
+import os
+from mongodb import MongoClient
+
+mongo_key = os.getenv('mongo_key')
+client = MongoClient(mongo_key)
+
+database = 'Chats'
+collection_name = 'OpenRouter'
+collection = client[database][collection_name]
