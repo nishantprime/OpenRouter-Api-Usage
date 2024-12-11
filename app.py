1a52913452487669e1cd8d213e2e07b07e01b611
nishantprime <nishantkumarprime@gmail.com>
2024-12-11 11:21:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,14 @@ app = Flask(__name__)
 @app.route('/', methods = ['GET', 'POST'])
 def chat():
   if request.method == 'GET':
-    chat = collection.find()
+    chat = collection.find({},{'prompt':1, 'response':1, 'formatted_time':1})
     return render_template('index.html', chat=chat)
   else:
     prompt = request.form['prompt']
-    response = generation(prompt)
-    return render_template('index.html', response = response)
+    time, response, token_usage = generation(prompt)
+    if not time :
+      return  render_template('index.html', error=response)
+    return render_template('index.html', time=time, response=response, token_usage=token_usage)
 
 
 if __name__=='__main__':
